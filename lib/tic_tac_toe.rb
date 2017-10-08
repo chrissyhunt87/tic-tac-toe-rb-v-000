9178ed4a12f5f68a4d23306d5639f0637b3ba58b
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-08 21:33:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,18 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #left col
+  [1,4,7], #middle col
+  [2,5,8], #right col
+  [0,4,8], #l to r diagonal
+  [2,4,6] #r to l diagonal
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
